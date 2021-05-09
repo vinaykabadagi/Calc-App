8b106ffaa66e457b6d11e32d29142a79a5cd1f45
Vinay Kabadagi <vinaykabadagi@gmail.com>
2021-05-09 20:57:16 +0530

    1

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.calc;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
